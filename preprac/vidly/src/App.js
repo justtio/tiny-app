3a85952d19f212a8946c266d8e412e70beef39ba
justtio <leviknows@gmial.com>
2019-12-11 10:04:16 +0800

    fix some iusses

--- preprac/vidly/src/App.js
+++ preprac/vidly/src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/Navbar";
+import MovieForm from "./components/movieForm";
 
 function App() {
   return (
-    <main className="container">
-      <Route path="/movies" component={Movies} />
-      <Route path="/customers" component={Customers} />
-      <Route path="/rentals" component={Rentals} />
-      <Route path="/not-found" component={NotFound} />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
--- preprac/vidly/src/components/customers.jsx
+++ preprac/vidly/src/components/customers.jsx
@@ -3,3 +3,5 @@ import React from "react";
 const Customers = () => {
   return <h1>Customers</h1>;
 };
+
+export default Customers;
