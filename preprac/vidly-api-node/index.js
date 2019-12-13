f90def5b91ba830465c323e9c8b3ef29296f47e3
justtio <leviknows@gmial.com>
2019-12-13 12:11:12 +0800

    almost done

--- preprac/vidly-api-node/index.js
+++ preprac/vidly-api-node/index.js
@@ -1,9 +1,9 @@
-const winston = require("winston");
+// const winston = require("winston");
 const express = require("express");
 const config = require("config");
 const app = express();
 
-require("./startup/logging")();
+// require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
@@ -12,7 +12,7 @@ require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
-  winston.info(`Listening on port ${port}...`)
+  console.log(`Listening on port ${port}...`)
 );
 
 module.exports = server;
